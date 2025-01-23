83508fcfaf6d124c69d51b38597485112284f3be
Alex <Lam.Alexander@hotmail.com>
2025-01-23 01:24:17 -0700

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Route, Routes,BrowserRouter } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
