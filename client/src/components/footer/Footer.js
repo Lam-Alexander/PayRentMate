e320dd4710de0b46004cf5529da5b25514976ed4
Alex <Lam.Alexander@hotmail.com>
2025-05-14 15:20:38 -0600

    style: adjusted styling for price section / card

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,6 +1,22 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div>
+        <div>
+          <h2>Do you have question?</h2>
+          <p>
+            If you have any questions or would like more information <br /> one
+            of our advirsor is excited to help you
+          </p>
+          <button>Contact us</button>
+        </div>
+        <div>
+          <p>2025 all rights reserve</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
